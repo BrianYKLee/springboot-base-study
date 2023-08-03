8742da585244e52e2fede76d65b23a106816598c
BrianYKLee <yk95013@gmail.com>
2023-08-03 22:31:30 +0900

    세팅 완료

---
+++ src/main/java/com/study/base/boot/aggregations/v1/order/presentation/OrderController.java
@@ -0,0 +1,15 @@
+package com.study.base.boot.aggregations.v1.order.presentation;
+
+import com.study.base.boot.config.annotations.RestAPI;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RestAPI("/v1/orders")
+public class OrderController {
+
+    @GetMapping
+    public List<String> getOrders(){
+        return List.of("A", "B", "C");
+    }
+}
