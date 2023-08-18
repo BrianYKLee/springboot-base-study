6343373d6642ed0f05644fe9745c98075ec1eadf
BrianYKLee <yk95013@gmail.com>
2023-08-19 00:30:39 +0900

    숙제(3) 수정

--- src/main/java/com/study/base/boot/SpringbootBaseStudyApplication.java
+++ src/main/java/com/study/base/boot/SpringbootBaseStudyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class tudyApplication {
+public class SpringbootBaseStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootBaseStudyApplication.class, args);
