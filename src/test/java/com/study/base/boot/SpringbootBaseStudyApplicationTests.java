42d6e2dc851626e2ccfef58a6bb3d19d1fd9d24b
BrianYKLee <yk95013@gmail.com>
2023-08-22 07:07:03 +0900

    숙제(3)

---
+++ src/test/java/com/study/base/boot/SpringbootBaseStudyApplicationTests.java
@@ -0,0 +1,19 @@
+package com.study.base.boot;
+
+import com.study.base.boot.aggregations.v1.order.application.dto.req.CreateOrder;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBaseStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void createOrders(){
+
+    }
+
+}
